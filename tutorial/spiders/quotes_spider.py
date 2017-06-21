2f4a8531107712aa8468876185239e68425d4a2d
ezirmusitua <jferroal@gmail.com>
2017-06-21 21:54:52 +0800

    Add ignore

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -14,7 +14,7 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        filename = 'result/quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
