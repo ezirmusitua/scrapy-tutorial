3c1a4c4c2e5a60fa3ed566cd2fc858b2a9024fb9
ezirmusitua <jferroal@gmail.com>
2017-06-22 21:39:53 +0800

    Add use selector example

---
+++ selector/use-selector.py
@@ -0,0 +1,17 @@
+from scrapy.http import HtmlResponse
+
+response = HtmlResponse(url='http://doc.scrapy.org/en/latest/_static/selectors-sample1.httml')
+
+# querying with xpath
+print('with xpath: ', response.xpath('//title/text()'))
+# querying with css selector
+print('with css: ', response.css('title::text'))
+# querying with css and xpath
+print('with css & xpath: ', response.css('img').xpath('@src').extract())
+# extract text only
+print('extract text only|xpath: ', response.xpath('//title/text()').extract())
+print('extract text only|css: ', response.css('title::text'))
+# extract the first
+print('extract the first: ', response.xpath('//title/text()').extract_first())
+print('extract the first with default: ', response.xpath('//title/p/text()').extract_first(default='not-found'))
+
