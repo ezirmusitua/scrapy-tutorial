5ba4a1e49d52db7af5330ae857a1471a3b715acc
ezirmusitua <jferroal@gmail.com>
2017-06-24 11:15:15 +0800

    Add pipeline example 2

--- pipeline/pipeline/pipelines.py
+++ pipeline/pipeline/pipelines.py
@@ -4,11 +4,26 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+
 from scrapy.exceptions import DropItem
 
 
 class RemoveLovePipeline(object):
     def process_item(self, item, spider):
         if 'love' in item['tags']:
-            DropItem('No love we need')
+            raise DropItem('No love we need')
+        return item
+
+
+class JsonWriterPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('items.jl', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
         return item
