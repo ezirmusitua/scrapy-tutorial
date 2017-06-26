18bd7770da0406745b7776e8bd896abf41334d7e
ezirmusitua <jferroal@gmail.com>
2017-06-26 21:37:02 +0800

    Add csv exporter example

--- item_exporter/item_exporter/pipelines.py
+++ item_exporter/item_exporter/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy import signals
-from scrapy.exporters import XmlItemExporter
+from scrapy.exporters import XmlItemExporter, CsvItemExporter, PickleItemExporter
 
 
 class XmlExportPipeline(object):
@@ -13,6 +13,10 @@ class XmlExportPipeline(object):
         self.files = dict()
         self.exporter = None
 
+    # this classmethod is called to create a pipeline instance from a Crawler
+    # It must return a new instance of the pipeline
+    # Crawler object provides access to all Scrapy core components like settings and signals
+    # It is a way for pipeline to access them and hook its functionality into Scrapy
     @classmethod
     def from_crawler(cls, crawler):
         pipeline = cls()
@@ -29,6 +33,34 @@ class XmlExportPipeline(object):
     def spider_closed(self, spider):
         self.exporter.finish_exporting()
         file = self.files.pop(spider)
+        file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+
+class CsvExportPipeline(object):
+    def __init__(self):
+        self.files = dict()
+        self.exporter = None
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        pipeline = cls()
+        crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
+        crawler.signals.connect(pipeline.spider_closed, signals.spider_closed)
+        return pipeline
+
+    def spider_opened(self, spider):
+        file = open('%s.csv' % spider.name, 'w+b')
+        self.files[spider] = file
+        self.exporter = CsvItemExporter(file)
+        self.exporter.start_exporting()
+
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        file = self.files.pop()
         file.close()
 
     def process_item(self, item, spider):
