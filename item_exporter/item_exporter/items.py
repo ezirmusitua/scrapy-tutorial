dfc1d3c54a7fd1a0d91184101d0b77645dc53848
ezirmusitua <jferroal@gmail.com>
2017-06-26 15:10:17 +0800

    Add item_exporter.xml example

---
+++ item_exporter/item_exporter/items.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+import scrapy
+from scrapy.loader.processors import Join, TakeFirst
+
+
+def strip_input(values):
+    return map(lambda x: str.strip(x), values)
+
+
+class QuoteItem(scrapy.Item):
+    author = scrapy.Field(
+        input_processor=strip_input,
+        output_processor=TakeFirst()
+    )
+    author_link = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    text = scrapy.Field(
+        input_processor=strip_input,
+        output_processor=Join()
+    )
+    text_hash = scrapy.Field()
+    tags = scrapy.Field()
+    screen_shot_filename = scrapy.Field()
